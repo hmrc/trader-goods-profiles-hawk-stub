82f30cbc4fbefc68860a47f2f09901d8721f1e17
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2024-05-16 07:56:07 +0000

    'Creating new service trader-goods-profiles-hawk-stub'

---
+++ app/uk/gov/hmrc/tradergoodsprofileshawkstub/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.tradergoodsprofileshawkstub.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
