f554299fc0734a919ab10df3d3e5da36b170c6ba
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-05-20 14:50:11 +0100

    Add GoodsItem models

---
+++ app/uk/gov/hmrc/tradergoodsprofileshawkstub/models/GoodsItemMetadata.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofileshawkstub.models
+
+import play.api.libs.json.{Format, Json, OFormat}
+import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
+
+import java.time.Instant
+
+final case class GoodsItemMetadata(
+                                    version: Int,
+                                    active: Boolean,
+                                    locked: Boolean,
+                                    toReview: Boolean,
+                                    reviewReason: Option[String],
+                                    declarable: String,
+                                    ukimsNumber: Option[String],
+                                    nirmsNumber: Option[String],
+                                    niphlNumber: Option[String],
+                                    srcSystemName: String,
+                                    createdDateTime: Instant,
+                                    updatedDateTime: Instant
+                                  )
+
+object GoodsItemMetadata {
+
+  implicit lazy val format: OFormat[GoodsItemMetadata] = Json.format
+
+  lazy val mongoFormat: OFormat[GoodsItemMetadata] = {
+    implicit val instantFormat: Format[Instant] = MongoJavatimeFormats.instantFormat
+    Json.format
+  }
+}
---
+++ app/uk/gov/hmrc/tradergoodsprofileshawkstub/models/GoodsItemRecord.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofileshawkstub.models
+
+import play.api.libs.json.{Json, OFormat}
+
+final case class GoodsItemRecord(
+                                  recordId: String,
+                                  goodsItem: GoodsItem,
+                                  metadata: GoodsItemMetadata
+                                )
+
+object GoodsItemRecord {
+
+  implicit lazy val format: OFormat[GoodsItemRecord] = Json.format
+
+  lazy val mongoFormat: OFormat[GoodsItemRecord] = {
+    implicit val goodsItemFormat: OFormat[GoodsItem] = GoodsItem.mongoFormat
+    implicit val goodsItemMetadataFormat: OFormat[GoodsItemMetadata] = GoodsItemMetadata.mongoFormat
+    Json.format
+  }
+}
