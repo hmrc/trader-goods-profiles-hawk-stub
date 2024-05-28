5c22588ba5625ec57360eb184ca379cbe5f47e92
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-05-28 12:59:47 +0100

    Add SchemaValidationService

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,8 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "com.beachape"            %% "enumeratum-play"            % "1.8.0",
-    "org.typelevel"           %% "cats-core"                  % "2.10.0"
+    "org.typelevel"           %% "cats-core"                  % "2.10.0",
+    "com.github.erosb"        %  "everit-json-schema"         % "1.14.4"
   )
 
   val test = Seq(
