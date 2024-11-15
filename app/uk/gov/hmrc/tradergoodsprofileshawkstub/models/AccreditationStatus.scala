1b18fe15b950fdc46f740e1008c51ebbbf8fbd45
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-11-15 11:04:29 +0000

    TGP-2887: update accreditation status

--- app/uk/gov/hmrc/tradergoodsprofileshawkstub/models/AccreditationStatus.scala
+++ app/uk/gov/hmrc/tradergoodsprofileshawkstub/models/AccreditationStatus.scala
@@ -26,9 +26,11 @@ object AccreditationStatus extends PlayEnum[AccreditationStatus] {
 
   case object NotRequested extends AccreditationStatus("Not Requested")
   case object Requested extends AccreditationStatus("Requested")
+  case object Withdrawn extends AccreditationStatus("Advice withdrawn")
+  case object NotProvided extends AccreditationStatus("Advice not provided")
   case object InProgress extends AccreditationStatus("In progress")
-  case object InformationRequested extends AccreditationStatus("Information Requested")
-  case object Withdrawn extends AccreditationStatus("Withdrawn")
+  case object InformationRequested extends AccreditationStatus("Information requested")
+  case object AdviceReceived extends AccreditationStatus("Advice received")
   case object Approved extends AccreditationStatus("Approved")
   case object Rejected extends AccreditationStatus("Rejected")
 }
