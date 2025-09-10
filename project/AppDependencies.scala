0d44f30d0130f6d769ba3a46be0f4bc2bc71e48c
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-09-10 10:26:27 +0100

    TGP-3571 Update Depenendcies (#65)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.16.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.19.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "com.beachape"            %% "enumeratum-play"            % "1.9.0",
-    "org.typelevel"           %% "cats-core"                  % "2.13.0",
-    "com.github.erosb"        %  "everit-json-schema"         % "1.14.6"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
+    "com.beachape"      %% "enumeratum-play"           % "1.9.0",
+    "org.typelevel"     %% "cats-core"                 % "2.13.0",
+    "com.github.erosb"   % "everit-json-schema"        % "1.14.6"
   )
 
   val test: Seq[ModuleID] = Seq(
-  "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-  "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test
   )
 
   val it = Seq.empty
